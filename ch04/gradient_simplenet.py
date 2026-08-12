f7fe625ec67f64fa8da1d5e33d1633682b5be8e7
Shota Matsubara <baramatsubonzo@gmail.com>
2026-08-12 21:38:21 +0900

    勾配

---
+++ ch04/gradient_simplenet.py
@@ -0,0 +1,19 @@
+import numpy as np
+import sys, os
+sys.path.append(os.pardir)  # 親ディレクトリのファイル
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
